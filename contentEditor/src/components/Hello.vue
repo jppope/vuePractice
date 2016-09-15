9f1a0e15649d6302235ed7d82232e1ec9e181d83
jppope <jonpaul@lojistic.com>
2016-09-15 14:25:06 -0700

    tester project

---
+++ contentEditor/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+  	<div class="column medium-10 medium-offset-1">
+	  <div class="hello">
+	    <h1>{{ msg }}</h1>
+	  </div>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
---
+++ contentEditor/src/components/keyword.vue
@@ -0,0 +1,20 @@
+<template>
+  <keyword>
+  <div class="row">
+  	<div class="column medium-10 medium-offset-1">
+      <input name="keyword" v-model="keyword">
+	   {{ keyword }}
+	</div?
+  </div>
+  </keyword>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      keyword: ''
+    }
+  }
+}
+</script>
